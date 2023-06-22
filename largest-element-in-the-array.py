28c16d66d80974d2e0d613cd1da1fa8aae355d28
LOKESH4423 <112263800+LOKESH4423@users.noreply.github.com>
2023-06-22 17:15:16 +0530

    largest element

--- largest-element-in-the-array.py
+++ largest-element-in-the-array.py
@@ -1,6 +1,5 @@
-def largestElement(arr, n):
-    max_val = arr[0]
-    for i in range(1, n):
-        if arr[i] > max_val:
-            max_val = arr[i]
+def largestElement(arr):
+    max_val=arr[0]
+    for i in range(len(arr)):
+        max_val=max(max_val,arr[i])
     return max_val
